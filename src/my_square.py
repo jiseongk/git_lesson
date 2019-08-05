6b067d4130f5d0b7c0a833ad7092ac50910c4f67
Ji Seong Kim <ji.seong@outlook.com>
2019-08-05 13:22:06 +0800

    x -> y and square 42

--- src/my_square.py
+++ src/my_square.py
@@ -1,7 +1,7 @@
-def my_square(x):
+def my_square(y):
     """ take a value and returns the square value.
     use the ** operator
     """ 
-    return ( x ** 2)
+    return ( y ** 2)
 
-print (my_square(4))
+print (my_square(42))
