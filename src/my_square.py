2b90fc280614bee0e2e23a43d83c606a7209011b
jiseongk <ji.seong@outlook.com>
2019-08-05 13:27:46 +0800

    x -> z and square 24

--- src/my_square.py
+++ src/my_square.py
@@ -1,7 +1,7 @@
-def my_square(x):
+def my_square(z):
     """ take a value and returns the square value.
     use the ** operator
     """ 
-    return ( x ** 2)
+    return ( z * z)
 
-print (my_square(4))
+print (my_square(24))
